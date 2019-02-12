4c01ea07d51b8d8b7582f336b9568fb62609febc
WestleyK <westley@sylabs.io>
2019-02-11 17:46:57 -0800

    Updated example/Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
-
+# Created by: WestleyR
+# email: westleyr@nym.hush.com
+# Date: Feb 11, 2019
+# https://github.com/WestleyR/arglib
+# Version-1.0.0
+#
+# The Clear BSD License
+#
+# Copyright (c) 2019 WestleyR
+# All rights reserved.
+#
+# This software is licensed under a Clear BSD License.
+#
 
 CC = gcc
 TARGET = test
@@ -17,11 +29,15 @@ CLIB = clib install
 .PHONY:
 all: $(TARGET)
 
+.PHONY:
 $(TARGET): $(FUNC) $(SOURCE)
 	$(CC) -o $(TARGET) $(SOURCE) $(FUNC)
 
-
+.PHONY:
 $(FUNC):
 	$(CLIB) $(DEP_URL)
 
-
+.PHONY:
+clean:
+	rm -f $(TARGET)
+	rm -rf deps
--- example.c
+++ example.c
@@ -1,8 +1,8 @@
 // created by: WestleyR
 // email: westleyr@nym.hush.com
 // https://github.com/WestleyR/arglib
-// date: Jan 17, 2018
-// version-1.0.0
+// date: Feb 11, 2019
+// version-1.0.1
 //
 // The Clear BSD License
 //
@@ -24,17 +24,19 @@
 
 char *SCRIPT_NAME;
 char *Path;
+char *type;
 
 void helpMenu() {
     printf("DESCRIPTION:\n");
-    printf("  list file with relvent path, used for definging varibals, eg.\n");
-    printf("  FOO = $(shell lcf --type=.c libs/)  # 'FOO' will be a string if all .c files.\n");
+    printf("  this is a test example for arglib.\n");
     printf("\n");
     printf("USAGE:\n");
     printf("  %s [--option]... <path>\n", SCRIPT_NAME);
     printf("\n");
     printf("OPTIONS:\n");
-    printf("  --type=string  : list files only of type string.\n");
+    printf("  --type=string  : set type for something.\n");
+    printf("  -l, --list     : list.\n");
+    printf("  -f, --force    : force.\n");
     printf("  -h, --help     : print help menu & exit.\n");
     printf("  -v, --version  : print script version & exit.\n");
     printf("\n");
@@ -50,32 +52,33 @@ void scriptVersion() {
 int main(int argc, char** argv) {
     SCRIPT_NAME = argv[0];
 
-    char *var;
-    char *fileType;
+    int list;
+    int force;
 
     if (argc <= 1 ) {
         helpMenu();
     }
 
     for (int i=1; i < argc; i++) {
-        var = get_val(argv[i], TypeFile);
+        char* var = get_val(argv[i], TypeFile);
 
         if (check_flag(argv[i], "", "--help") == 0) {
             helpMenu();
             return(0);
         } else if (check_flag(argv[i], "", "--version") == 0) {
             scriptVersion();
+            return(0);
         } else if (var != NULL) {
-            fileType = var;
+            type = var;
         } else if ((strstr(argv[i], "--") == argv[i]) && (strstr(argv[i], TypeFile) != argv[i])) {
             printf("%s: Option not found: %s\n", SCRIPT_NAME, argv[i]);
             return(2);
-        } else {
+        } else if (strstr(argv[i], "-") != argv[i]) {
             Path = argv[i];
         }
 
         // all the avalibal sort options:
-        if (check_small_args(argv[i], "hv") != 0) {
+        if (check_small_args(argv[i], "lfhv") != 0) {
             return(1);
         }
 
@@ -87,15 +90,26 @@ int main(int argc, char** argv) {
             helpMenu();
             return(0);
         }
+        if (find_args(argv[i], "l") == 0) {
+            list = 1;
+        }
+        if (find_args(argv[i], "f") == 0) {
+            force = 1;
+        }
     }
 
-
-    if (Path == NULL) {
-        printf("Need a Path.\n");
-        return(1);
+    if (type != NULL) {
+        printf("Type: %s\n", type);
     }
-
-    printf("\n");
+    if (list == 1) {
+        printf("List: true\n");
+    }
+    if (force == 1) {
+        printf("Force: true\n");
+    }
+    if (Path != NULL) {
+        printf("Path: %s\n", Path);
+    }
 
 
     return(0);
